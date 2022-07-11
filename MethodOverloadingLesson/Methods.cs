b2a4ce34038891823b51b9c43ec4b25e8c3f8e44
dannypellatt <danielpellatt92@gmail.com>
2022-07-11 12:48:04 -0400

    initial commit

---
+++ MethodOverloadingLesson/Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MethodOverloadingLesson
+{
+    public class Methods
+    {
+
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            if (z != true)
+            {
+                return "ERROR: your bool was false";
+            }
+            else
+            {
+                if (x + y == 1)
+                {
+                    return ($"{x + y} dollar");
+                }
+                else
+                {
+                    return ($"{x + y} dollars");
+                }
+            }
+        }
+
+    }
+}
+
---
+++ MethodOverloadingLesson/Program.cs
@@ -0,0 +1,11 @@
+﻿using MethodOverloadingLesson;
+
+Console.WriteLine(Methods.Add(0, 1));
+
+Console.WriteLine(Methods.Add(1.5m, 1.7m));
+
+Console.WriteLine(Methods.Add(0, 1, true));
+
+Console.WriteLine(Methods.Add(5, 6, true));
+
+Console.WriteLine(Methods.Add(0, 1, false));
